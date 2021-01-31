161df15804f806475d0fbf75ad2736a01552f11f
Ramon van Sprundel <ramonvansprundel@gmail.com>
2021-01-31 13:19:39 +0100

    minimal kernel that prints hello world

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,29 @@ use core::panic::PanicInfo;
 
 #[no_mangle] // don't mangle name randomly
 pub extern "C" fn _start() -> ! {
+    print_string();
     // this is our entry point called in C
     // the linker selects function _start by default
-    loop{}
+    loop {}
+}
+
+fn print_string() {
+    // prints simple hello world to screen
+    static HELLO: &[u8] = b"Hello world!";
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
 }
 
 // this function is our panic handler
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
 
 
